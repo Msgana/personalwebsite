2e2cd9e7b512a2facd1afd691f80566df14bfc18
Misgana Gebremariam <msgana@Misganas-MacBook-Air.local>
2020-05-20 23:30:35 -0700

    initial

---
+++ src/Nav.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Nav = (props) => {
+  return <div>Navigation bar</div>;
+};
+
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./ContentCard.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return (
+    <div>
+      <div className="ui-container">
+        <img src={faker.image.avatar()} alt="a person"></img>
+        <h1>
+          {faker.name.findName()} {faker.name.lastName()}
+        </h1>
+        <h5>{faker.name.jobTitle()}</h5>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        <div className="ui-container-button">
+          <button className="ui-aboutMe-button">About.me</button>
+          <button className="ui-resume-button">Resume</button>
+        </div>
+
+        <div className="ui-container-logo">
+          <div className="ui-container-github">
+            <img
+              src="githubplus.png"
+              alt="Github"
+              height="50%"
+              width="50%"
+            ></img>
+          </div>
+          <div className="ui-container-linkedin">
+            <img
+              src="linkedin.svg"
+              alt="linkedin"
+              height="50%"
+              width="50%"
+            ></img>
+          </div>
+        </div>
+      </div>
+      <div className="ui-container-footer">
+        <span className="ui-container-footer-paragraph">
+          <p>{faker.address.city()}</p>
+          <p>
+            {faker.address.state()} {faker.address.countryCode()}{" "}
+            {faker.address.zipCode()}
+          </p>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
