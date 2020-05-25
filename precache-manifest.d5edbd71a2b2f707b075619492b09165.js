9adc93d2dc8c1ecc48a365102dc971518a4b1062
Msgana <misganayohannes@gmail.com>
2020-05-24 23:17:56 -0700

    Updates

---
+++ precache-manifest.d5edbd71a2b2f707b075619492b09165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3bdd9eacc23381bbeef96259af90f85",
+    "url": "/personalwebsite/index.html"
+  },
+  {
+    "revision": "c59183e542be9087414c",
+    "url": "/personalwebsite/static/css/main.ea150f21.chunk.css"
+  },
+  {
+    "revision": "4b9eaa9006d7ff8f319f",
+    "url": "/personalwebsite/static/js/2.88fbabcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personalwebsite/static/js/2.88fbabcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c59183e542be9087414c",
+    "url": "/personalwebsite/static/js/main.7a5497b7.chunk.js"
+  },
+  {
+    "revision": "6b02b8d030a11a15506f",
+    "url": "/personalwebsite/static/js/runtime-main.63e7c44e.js"
+  }
+]);
