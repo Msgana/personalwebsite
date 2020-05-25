4b20a4dcdd402606fb65c4570a149c69226fd5b5
Msgana <misganayohannes@gmail.com>
2020-05-25 13:32:17 -0700

    update

--- src/index.js
+++ src/index.js
@@ -7,12 +7,7 @@ const App = () => {
   return (
     <div>
       <div className="ui-container">
-        <img
-          src="profile-picture.png"
-          height="50%"
-          width="50%"
-          alt="a person"
-        ></img>
+        <img src={faker.image.avatar} alt="a person"></img>
         <h1>Misgana Gebremariam</h1>
         <h5>Computer Science</h5>
         <h4>Arizona State University</h4>
